700784b47606972276e16576d6bbc950dbce1bf0
unknown <zpfreeman14@gmail.com>
2018-08-13 12:03:38 -0500

    first commit

---
+++ java-client-handler/src/main/java/com/cooksys/java_client_handler_zac_freeman/App.java
@@ -0,0 +1,13 @@
+package com.cooksys.java_client_handler_zac_freeman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ java-client-handler/src/main/java/com/cooksys/java_client_handler_zac_freeman/Client.java
@@ -0,0 +1,5 @@
+package com.cooksys.java_client_handler_zac_freeman;
+
+public class Client {
+
+}
